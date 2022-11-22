a49433937ccdf3877f64915baac75462e252dde3
이동현 <@DESKTOP-DTF4VNI>
2022-11-22 16:47:01 +0900

    day04

---
+++ JavaSE/src/day03/loop/MainClass06.java
@@ -0,0 +1,18 @@
+package day03.loop;
+/*
+ * break 문
+ * 
+ *  
+ */
+
+public class MainClass06 {
+	public static void main(String[] args) {
+		for(int i=0; i<5; i++) {
+			if(i == 3) {
+				break;
+			}
+			System.out.println("i : " +i);
+		}
+	}
+
+}
